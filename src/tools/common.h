339fbd6da2010b26cb36e05a995614f43c423f38
Kawasss <112657018+Kawasss@users.noreply.github.com>
2024-01-03 18:14:46 +0100

    added back ReadFile

--- src/tools/common.h
+++ src/tools/common.h
@@ -4,6 +4,22 @@
 #include <iostream>
 #include <fstream>
 
+inline std::vector<char> ReadFile(const std::string& filePath)
+{
+	std::ifstream file(filePath, std::ios::ate | std::ios::binary);
+	if (!file.is_open())
+		throw std::runtime_error("Failed to open the file at " + filePath);
+
+	size_t fileSize = (size_t)file.tellg();
+	std::vector<char> buffer(fileSize);
+
+	file.seekg(0);
+	file.read(buffer.data(), fileSize);
+
+	file.close();
+	return buffer;
+}
+
 inline std::vector<std::string> SeparateStringByChar(const std::string& input, char separator)
 {
 	constexpr auto EMPTY_STRING = "";
