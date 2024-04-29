f591dd338c1f816e8d8fbe43cd67d4df8075e0c2
Kawasss <112657018+Kawasss@users.noreply.github.com>
2024-04-29 16:35:08 +0200

    reduced the ghosting issue

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -115,13 +115,11 @@ void Camera::UpdateUpAndRightVectors()
 
 glm::vec2 Camera::GetMotionVector()
 {
-	glm::vec4 NDC = GetProjectionMatrix() * GetViewMatrix() * glm::vec4(0, 0, 0, 1);
-	NDC /= NDC.w;
+	glm::vec4 NDC = GetProjectionMatrix() * GetViewMatrix() * glm::vec4(position, 1);
+	//NDC /= NDC.w + 0.000001f;
 
 	glm::vec2 current = glm::vec2((NDC.x + 1.0f) * 0.5f, (NDC.y + 1.0f) * 0.5f);
 	glm::vec2 ret = current - prev2D;
-	if (ret != ret) // if the player is at 0, 0 ret will be NaN, according to IEEE NaN cannot be equal to NaN so this checks for that
-		ret = glm::vec2(0);
 	prev2D = current;
 	return ret;
 }
