10607fe467feddf9811a58a96d61d81156e246cb
Kawasss <112657018+Kawasss@users.noreply.github.com>
2026-01-20 23:50:49 +0100

    removed macros from binarystream

--- src/BinaryStream.cpp
+++ src/BinaryStream.cpp
@@ -6,77 +6,10 @@ module IO.BinaryStream;
 
 import std;
 
-template<typename T>
-concept PrimitiveOnly = std::is_fundamental_v<T>;
-
-template<PrimitiveOnly T>
-void WritePrimitiveToVector(std::vector<char>& vec, const T& val)
-{
-	constexpr size_t writeCount = sizeof(T) / sizeof(std::vector<char>::value_type);
-	const char* pValue = reinterpret_cast<const char*>(&val);
-
-	vec.insert(vec.end(), pValue, pValue + writeCount);
-}
-
-template<PrimitiveOnly T>
-void ReadPrimitiveFromSpan(const std::span<char const>& vec, std::size_t& offset, T& val)
-{
-	constexpr size_t readCount = sizeof(T) / sizeof(std::vector<char>::value_type);
-	const T* pValue = reinterpret_cast<const T*>(&vec[0] + offset);
-	val = *pValue;
-
-	offset += readCount;
-}
-
 BinaryStream::BinaryStream(const std::vector<char>& data) : data(data)
 {
 
 }
-
-#define DEFINE_WRITE_OPERATOR(type)                     \
-BinaryStream& BinaryStream::operator<<(const type& val) \
-{                                                       \
-	WritePrimitiveToVector(data, val);                  \
-	return *this;                                       \
-}                                                       \
-
-#define DEFINE_READ_OPERATOR(type)                \
-BinaryStream& BinaryStream::operator>>(type& val) \
-{                                                 \
-	ReadPrimitiveFromSpan(data, offset, val);   \
-	return *this;                                 \
-}                                                 \
-
-DEFINE_WRITE_OPERATOR(std::uint64_t);
-DEFINE_WRITE_OPERATOR(std::uint32_t);
-DEFINE_WRITE_OPERATOR(std::uint16_t);
-DEFINE_WRITE_OPERATOR(std::uint8_t);
-
-DEFINE_WRITE_OPERATOR(std::int64_t);
-DEFINE_WRITE_OPERATOR(std::int32_t);
-DEFINE_WRITE_OPERATOR(std::int16_t);
-DEFINE_WRITE_OPERATOR(std::int8_t);
-
-DEFINE_WRITE_OPERATOR(float);
-DEFINE_WRITE_OPERATOR(bool);
-DEFINE_WRITE_OPERATOR(char);
-
-DEFINE_READ_OPERATOR(std::uint64_t);
-DEFINE_READ_OPERATOR(std::uint32_t);
-DEFINE_READ_OPERATOR(std::uint16_t);
-DEFINE_READ_OPERATOR(std::uint8_t);
-
-DEFINE_READ_OPERATOR(std::int64_t);
-DEFINE_READ_OPERATOR(std::int32_t);
-DEFINE_READ_OPERATOR(std::int16_t);
-DEFINE_READ_OPERATOR(std::int8_t);
-
-DEFINE_READ_OPERATOR(float);
-DEFINE_READ_OPERATOR(bool);
-DEFINE_READ_OPERATOR(char);
-
-#undef DEFINE_READ_OPERATOR
-#undef DEFINE_WRITE_OPERATOR
 
 void BinaryStream::Read(char* dst, std::size_t count)
 {
@@ -101,7 +34,7 @@ std::size_t BinaryStream::GetOffset() const
 	return offset;
 }
 
-BinarySpan::BinarySpan(const BinaryStream& stream) : data(stream.data)
+BinarySpan::BinarySpan(const BinaryStream& stream) : data(stream.data.begin(), stream.data.end())
 {
 
 }
@@ -115,29 +48,6 @@ BinarySpan::BinarySpan(const std::vector<char>& data) : data(data)
 {
 
 }
-
-#define DEFINE_READ_OPERATOR(type)                  \
-const BinarySpan& BinarySpan::operator>>(type& val) const \
-{                                                   \
-	ReadPrimitiveFromSpan(data, offset, val);       \
-	return *this;                                   \
-}        
-
-DEFINE_READ_OPERATOR(std::uint64_t);
-DEFINE_READ_OPERATOR(std::uint32_t);
-DEFINE_READ_OPERATOR(std::uint16_t);
-DEFINE_READ_OPERATOR(std::uint8_t);
-
-DEFINE_READ_OPERATOR(std::int64_t);
-DEFINE_READ_OPERATOR(std::int32_t);
-DEFINE_READ_OPERATOR(std::int16_t);
-DEFINE_READ_OPERATOR(std::int8_t);
-
-DEFINE_READ_OPERATOR(float);
-DEFINE_READ_OPERATOR(bool);
-DEFINE_READ_OPERATOR(char);
-
-#undef DEFINE_READ_OPERATOR
 
 void BinarySpan::Read(char* dst, size_t count) const
 {
--- src/io/BinaryStream.ixx
+++ src/io/BinaryStream.ixx
@@ -2,8 +2,8 @@ export module IO.BinaryStream;
 
 import std;
 
-#define DECLARE_WRITE_OPERATOR(type) BinaryStream& operator<<(const type& val)
-#define DECLARE_READ_OPERATOR(type)  BinaryStream& operator>>(type& val)
+template<typename T>
+concept PrimitiveOnly = std::is_fundamental_v<T>;
 
 export class BinaryStream
 {
@@ -11,33 +11,26 @@ public:
 	BinaryStream() = default;
 	BinaryStream(const std::vector<char>& data); // copies the vector, does not assume ownership
 
-	DECLARE_WRITE_OPERATOR(std::uint64_t);
-	DECLARE_WRITE_OPERATOR(std::uint32_t);
-	DECLARE_WRITE_OPERATOR(std::uint16_t);
-	DECLARE_WRITE_OPERATOR(std::uint8_t);
+	template<PrimitiveOnly T>
+	BinaryStream& operator<<(const T& val)
+	{
+		constexpr size_t writeCount = sizeof(T) / sizeof(std::vector<char>::value_type);
+		const char* pValue = reinterpret_cast<const char*>(&val);
 
-	DECLARE_WRITE_OPERATOR(std::int64_t);
-	DECLARE_WRITE_OPERATOR(std::int32_t);
-	DECLARE_WRITE_OPERATOR(std::int16_t);
-	DECLARE_WRITE_OPERATOR(std::int8_t);
+		Write(pValue, writeCount);
+		return *this;
+	}
 
-	DECLARE_WRITE_OPERATOR(float);
-	DECLARE_WRITE_OPERATOR(bool);
-	DECLARE_WRITE_OPERATOR(char);
+	template<PrimitiveOnly T>
+	BinaryStream& operator>>(T& val)
+	{
+		constexpr size_t readCount = sizeof(T) / sizeof(std::vector<char>::value_type);
+		const T* pValue = reinterpret_cast<const T*>(&data[0] + offset);
+		val = *pValue;
 
-	DECLARE_READ_OPERATOR(std::uint64_t);
-	DECLARE_READ_OPERATOR(std::uint32_t);
-	DECLARE_READ_OPERATOR(std::uint16_t);
-	DECLARE_READ_OPERATOR(std::uint8_t);
-
-	DECLARE_READ_OPERATOR(std::int64_t);
-	DECLARE_READ_OPERATOR(std::int32_t);
-	DECLARE_READ_OPERATOR(std::int16_t);
-	DECLARE_READ_OPERATOR(std::int8_t);
-
-	DECLARE_READ_OPERATOR(float);
-	DECLARE_READ_OPERATOR(bool);
-	DECLARE_READ_OPERATOR(char);
+		offset += readCount;
+		return *this;
+	}
 
 	void Read(char* dst, size_t count);
 	void Write(const char* src, size_t count); // appends
@@ -51,11 +44,6 @@ private:
 	std::size_t offset = 0; // only used for reading
 };
 
-#undef DECLARE_READ_OPERATOR
-#undef DECLARE_WRITE_OPERATOR
-
-#define DECLARE_READ_OPERATOR(type)  const BinarySpan& operator>>(type& val) const
-
 export class BinarySpan
 {
 public:
@@ -63,28 +51,23 @@ public:
 	BinarySpan(const std::span<char const>& data);
 	BinarySpan(const std::vector<char>& data);
 
-	DECLARE_READ_OPERATOR(std::uint64_t);
-	DECLARE_READ_OPERATOR(std::uint32_t);
-	DECLARE_READ_OPERATOR(std::uint16_t);
-	DECLARE_READ_OPERATOR(std::uint8_t);
+	template<PrimitiveOnly T>
+	const BinarySpan& operator>>(T& val) const
+	{
+		constexpr size_t readCount = sizeof(T) / sizeof(std::vector<char>::value_type);
+		const T* pValue = reinterpret_cast<const T*>(&data[0] + offset);
+		val = *pValue;
 
-	DECLARE_READ_OPERATOR(std::int64_t);
-	DECLARE_READ_OPERATOR(std::int32_t);
-	DECLARE_READ_OPERATOR(std::int16_t);
-	DECLARE_READ_OPERATOR(std::int8_t);
-
-	DECLARE_READ_OPERATOR(float);
-	DECLARE_READ_OPERATOR(bool);
-	DECLARE_READ_OPERATOR(char);
+		offset += readCount;
+		return *this;
+	}
 
 	void Read(char* dst, std::size_t count) const;
 
-	std::span<char const> data;
+	std::span<const char> data;
 
 	size_t GetOffset() const;
 
 private:
 	mutable size_t offset = 0; // i think its fine to make this mutable since this variable is only ever used by the class itself and actual important part (the data) is never touched anyway
 };
-
-#undef DECLARE_READ_OPERATOR
